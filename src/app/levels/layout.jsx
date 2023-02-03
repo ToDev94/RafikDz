36b9a393bffd6ac1f1abb53a07b21d679aa796e5
ToDev94 <nefoucitoufik@gmail.com>
2023-02-03 12:40:48 +0100

    Solving the UI dosen't match problem in nav items 1

--- src/app/levels/layout.jsx
+++ src/app/levels/layout.jsx
@@ -146,27 +146,9 @@ const ArabicAccordion = () => {
 
         <AccordionPanel textAlign="right" w="100%" p={0}>
           <Flex direction="column" gap={1}>
-            <Link
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              href="#"
-              w="100%"
-              transition="all 0.3s"
-              _hover={{ bg: "gray.600" }}
-              px={2}
-              py={2}
-            >
-              <Flex alignItems="center" gap={4}>
-                <FiLink />
-                <Text size="xs">ثانوي اولي</Text>
-              </Flex>
-              <Badge colorScheme="teal">1587</Badge>
-            </Link>
-
-            {/* <NavigationItem num={8123}> الأولى ثانوي </NavigationItem>
+            <NavigationItem num={8123}> الأولى ثانوي </NavigationItem>
             <NavigationItem num={4523}> الثانية ثانوي </NavigationItem>
-            <NavigationItem num={2423}> الثالثة ثانوي </NavigationItem> */}
+            <NavigationItem num={2423}> الثالثة ثانوي </NavigationItem>
           </Flex>
         </AccordionPanel>
       </AccordionItem>
